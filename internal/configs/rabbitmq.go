dc54853dad44b15fbba31eac719d9edb9d9cb6f6
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-02 00:59:38 +0700

    update config rabbitmq

--- internal/configs/rabbitmq.go
+++ internal/configs/rabbitmq.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/phuslu/log"
+	"github.com/rabbitmq/amqp091-go"
 	"github.com/streadway/amqp"
 
 	"github.com/bagasunix/ngewarung/pkg/configs"
@@ -13,7 +14,7 @@ import (
 	"github.com/bagasunix/ngewarung/pkg/errors"
 )
 
-func InitRabbitMQ(ctx context.Context, cfg *env.Cfg, logger *log.Logger) *amqp.Connection {
+func InitRabbitMQ(ctx context.Context, cfg *env.Cfg, logger *log.Logger) *amqp091.Connection {
 	CfgBuild := &configs.DBConfig{
 		Driver:   cfg.RabbitMQ.Driver,
 		Host:     cfg.RabbitMQ.Host,
@@ -25,9 +26,9 @@ func InitRabbitMQ(ctx context.Context, cfg *env.Cfg, logger *log.Logger) *amqp.C
 }
 
 // InitRabbitMQ initializes and returns a RabbitMQ connection
-func NewPRabbitMQDB(ctx context.Context, cfg *configs.DBConfig, logger *log.Logger) *amqp.Connection {
+func NewPRabbitMQDB(ctx context.Context, cfg *configs.DBConfig, logger *log.Logger) *amqp091.Connection {
 	// Membuat DSN RabbitMQ
-	conn, err := amqp.DialConfig(cfg.GetDSN(), amqp.Config{
+	conn, err := amqp091.DialConfig(cfg.GetDSN(), amqp091.Config{
 		Heartbeat: 10 * time.Second,
 		Locale:    "en_US",
 		Dial:      amqp.DefaultDial(30 * time.Second),
@@ -36,13 +37,4 @@ func NewPRabbitMQDB(ctx context.Context, cfg *configs.DBConfig, logger *log.Logg
 
 	logger.Info().Msg("Connected to RabbitMQ")
 	return conn
-}
-
-// Membuat channel
-func NewRabbitMQChannel(conn *amqp.Connection, logger *log.Logger) *amqp.Channel {
-	ch, err := conn.Channel()
-	if err != nil {
-		logger.Fatal().Err(err).Msg("fail❌ Failed to open RabbitMQ channel")
-	}
-	return ch
 }
