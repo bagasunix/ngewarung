9e2321a72d09643225980a5d1fde16da104be611
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-02 18:36:40 +0700

    update request user

--- internal/delivery/dto/requests/user.go
+++ internal/delivery/dto/requests/user.go
@@ -7,25 +7,25 @@ import (
 )
 
 type UserRequest struct {
-	MerchantID *int64  `json:"merchant_id"`
-	OutletID   *int64  `json:"outlet_id"`
-	RoleID     int64   `json:"role_id"`
-	Name       string  `json:"name"`
-	Sex        int8    `json:"sex"`
-	Phone      string  `json:"phone"`
-	Email      string  `json:"email"`
-	Address    string  `json:"address"`
-	DOB        *string `json:"dob"`
-	Username   string  `json:"username"`
-	Password   string  `json:"password"`
-	Photo      *string `json:"photo"`
+	MerchantID *int64 `json:"merchant_id"`
+	OutletID   *int64 `json:"outlet_id"`
+	// RoleID     int64   `json:"role_id"`
+	Name     string  `json:"name"`
+	Sex      int     `json:"sex"`
+	Phone    string  `json:"phone"`
+	Email    string  `json:"email"`
+	Address  string  `json:"address"`
+	DOB      *string `json:"dob"`
+	Username string  `json:"username"`
+	Password string  `json:"password"`
+	Photo    *string `json:"photo"`
 }
 
 func (r UserRequest) Validate() error {
 	return validation.ValidateStruct(&r,
-		validation.Field(&r.RoleID, validation.Required),
+		// validation.Field(&r.RoleID, validation.Required),
 		validation.Field(&r.Name, validation.Required, validation.Length(2, 150)),
-		validation.Field(&r.Sex, validation.In(1, 2)),
+		validation.Field(&r.Sex, validation.Required, validation.In(1, 2)),
 		validation.Field(&r.Email, validation.Required, validation.Length(5, 150), validation.Match(regexp.MustCompile(`^[a-zA-Z0-9._%%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)).Error("invalid email format")),
 		validation.Field(&r.Username, validation.Required, validation.Length(3, 50)),
 		validation.Field(&r.Password, validation.Required, validation.Length(6, 0)),
