ce69fb1c73dc6a9de3f925d0d5b5f1cdc07b993a
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-03 10:36:29 +0700

    update helper

---
+++ pkg/helpers/string.go
@@ -0,0 +1,19 @@
+package helpers
+
+import (
+	"bytes"
+	"text/template"
+)
+
+func ParseTemplate(templateFileName string, data interface{}) (*string, error) {
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return nil, err
+	}
+	body := buf.String()
+	return &body, nil
+}
