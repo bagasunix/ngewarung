9bc14b437e90295d1fcea40ef1094977727f4ec6
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-04 23:57:17 +0700

    update helpers

--- pkg/helpers/mail.go
+++ pkg/helpers/mail.go
@@ -1,54 +1,54 @@
 package helpers
 
 import (
-	"fmt"
+	"crypto/tls"
 
-	mailjet "github.com/mailjet/mailjet-apiv3-go"
+	"gopkg.in/gomail.v2"
 
 	"github.com/bagasunix/ngewarung/pkg/env"
 )
 
 func SendEmail(body *string, to, subject string, s *env.Cfg) error {
 
-	mj := mailjet.NewMailjetClient(
-		s.Server.MailJet.ApiKey,
-		s.Server.MailJet.ScretKey,
-	)
+	// mj := mailjet.NewMailjetClient(
+	// 	s.Server.MailJet.ApiKey,
+	// 	s.Server.MailJet.ScretKey,
+	// )
 
-	messages := mailjet.MessagesV31{
-		Info: []mailjet.InfoMessagesV31{
-			{
-				From: &mailjet.RecipientV31{
-					Email: "no-reply@bagasunix.com",
-					Name:  "Ngewarung",
-				},
-				To: &mailjet.RecipientsV31{
-					mailjet.RecipientV31{Email: to},
-				},
-				Subject:  "Verifikasi Akun Ngewarung",
-				TextPart: "Klik link berikut untuk verifikasi akun Anda.",
-				HTMLPart: *body,
-			},
-		},
-	}
-	_, err := mj.SendMailV31(&messages)
-	if err != nil {
-		return fmt.Errorf("gagal kirim email: %w", err)
-	}
-
-	// m := gomail.NewMessage()
-	// m.SetHeader("From", "Ngewarung <"+s.App.Name+">")
-	// m.SetHeader("To", to)
-
-	// m.SetHeader("Subject", subject)
-
-	// m.SetBody("text/html", *body)
-
-	// mailDialer := gomail.NewDialer("in-v3.mailjet.com", 587, s.Server.MailJet.ApiKey, s.Server.MailJet.ScretKey)
-	// mailDialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-	// if err := mailDialer.DialAndSend(m); err != nil {
-	// 	return err
+	// messages := mailjet.MessagesV31{
+	// 	Info: []mailjet.InfoMessagesV31{
+	// 		{
+	// 			From: &mailjet.RecipientV31{
+	// 				Email: "no-reply@bagasunix.com",
+	// 				Name:  "Ngewarung",
+	// 			},
+	// 			To: &mailjet.RecipientsV31{
+	// 				mailjet.RecipientV31{Email: to},
+	// 			},
+	// 			Subject:  "Verifikasi Akun Ngewarung",
+	// 			TextPart: "Klik link berikut untuk verifikasi akun Anda.",
+	// 			HTMLPart: *body,
+	// 		},
+	// 	},
 	// }
+	// _, err := mj.SendMailV31(&messages)
+	// if err != nil {
+	// 	return fmt.Errorf("gagal kirim email: %w", err)
+	// }
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", "Ngewarung <no-reply@ngewarung.com>")
+	m.SetHeader("To", to)
+
+	m.SetHeader("Subject", subject)
+
+	m.SetBody("text/html", *body)
+
+	mailDialer := gomail.NewDialer(s.Server.MailJet.HostName, s.Server.MailJet.Port, s.Server.MailJet.Username, s.Server.MailJet.Password)
+	mailDialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := mailDialer.DialAndSend(m); err != nil {
+		return err
+	}
 
 	return nil
 }
--- pkg/helpers/string.go
+++ pkg/helpers/string.go
@@ -2,6 +2,8 @@ package helpers
 
 import (
 	"bytes"
+	"crypto/rand"
+	"math/big"
 	"text/template"
 )
 
@@ -16,4 +18,15 @@ func ParseTemplate(templateFileName string, data interface{}) (*string, error) {
 	}
 	body := buf.String()
 	return &body, nil
+}
+
+func GenerateRandomString(n int) string {
+	const letters = "012345678901234567890123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, _ := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret)
 }
